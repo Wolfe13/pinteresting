8cf01ae50d31f19df20b23f579dd6b0c9a973476
Lauren Wolfe <lawolfe@gmail.com>
2014-12-05 18:46:12 -0500

    add image upload with paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,6 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
-end
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates :image, :attachment_presence => true, :attachment_content_type => 
+  { :content_type => ["image/jpeg", "image/gif", "image/png"] }, :attachment_size => { :less_than => 4.megabytes }
+end 
